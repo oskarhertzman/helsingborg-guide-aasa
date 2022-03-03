5d68c05bf4a7903a6c11debfd1f4770ab8489d43
Oskar Hertzman <oskar.hertzman@hotmail.com>
2022-03-03 10:58:33 +0100

    added empty index.html on root

--- server.js
+++ server.js
@@ -5,10 +5,18 @@ const express = require('express');
 const app = new express();
 
 const PATH_TO_FILE = "/.well-known/apple-app-site-associtation"
+
+const PATH_TO_LANDING = "/.public/index.html"
+
+
 app.use(express.static(`./${PATH_TO_FILE}`));
 
 app.get(PATH_TO_FILE, (req, res) => {
     res.sendFile(path.resolve(__dirname, `.${PATH_TO_FILE}`));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, `.${PATH_TO_LANDING}`));
 });
 
 app.listen(4000, () => {
