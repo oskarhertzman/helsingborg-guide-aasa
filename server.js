d3d895c2e6a8a50cc00b82cce99f722678c4c886
Oskar Hertzman <oskar.hertzman@hotmail.com>
2022-03-03 10:09:00 +0100

    test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server');
+const express = require('express');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -6,5 +7,15 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('./well-known'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname,'/apple-app-site-associtation'));
+});
 
 server.listen(port);
